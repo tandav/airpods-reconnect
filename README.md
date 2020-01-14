71eaaddfe008a213a63d134e4a2227caf5715a1d
Alexander Rodionov <alx1dr@gmail.com>
2020-01-14 20:58:46 +0300

    kinda works

--- README.md
+++ README.md
@@ -4,3 +4,18 @@ brew install blueutil
 brew install switchaudio-osx
 python -m pip install PyUserInput
 ```
+
+
+logs example:
+```
+all good *
+all good  *
+triggered
+disconnect
+connect
+waiting until connect, current output device is: Built-in Output
+waiting until connect, current output device is: Built-in Output
+waiting until connect, current output device is: Built-in Output
+pressing PLAY
+all good  *
+```
--- main.py
+++ main.py
@@ -1,68 +1,54 @@
 import json
 import time
 import subprocess
-from pykeyboard import PyKeyboard # TODO: dive into source to get rid of PyUserInput dependency
+import random
+from pykeyboard import PyKeyboard # TODO: dive into source to get rid of PyUserInput dependency, also try osascript
+
+AIRPODS_NAME = 'AirPods Pro'
+
 
 cc = subprocess.check_call
 co = lambda x: subprocess.check_output(x, text=True).strip()
 
-AIRPODS_NAME = 'AirPods Pro'
 
-def find(name):
-    _ = co(['blueutil',  '--paired', '--format', 'json'])
-    _ = json.loads(_)
-    for x in _:
+def current_output():
+    return co(['SwitchAudioSource', '-c'])
+
+
+def get_id(name):
+    for x in json.loads(co(['blueutil',  '--paired', '--format', 'json'])):
         if x['name'] == name:
-            return x
-        
-def disconnect(id_): cc(['blueutil', '--disconnect', id_])
-def connect   (id_): cc(['blueutil', '--connect'   , id_])
-def is_connected(id_): return co(['blueutil',  '--is-connected', 'e4-76-84-20-f5-8d']) == '0'
+            return x['address']
 
 
-airpods = find(AIRPODS_NAME)
-'''
-airpods object looks like:
-{
-    'address': 'e4-76-84-20-f5-8d',
-    'recentAccessDate': '2020-01-14T08:58:45+03:00',
-    'paired': True,
-    'RSSI': 0,
-    'rawRSSI': -54,
-    'favourite': False,
-    'connected': True,
-    'name': 'AirPods Pro',
-    'slave': False,
-}
-'''
+def disconnect(device_id):
+    print('disconnect')
+    cc(['blueutil', '--disconnect', device_id])
 
-def current_output(): return co(['SwitchAudioSource', '-c'])
 
-# if current_output() != AIRPODS_NAME:
-#     print('triggered')
-#     disconnect(airpods['address'])
-#     connect(airpods['address'])
+def connect(device_id):
+    print('connect')
+    cc(['blueutil', '--connect'   , device_id])
+    while True:
+        curr_device = current_output()
+        if curr_device == AIRPODS_NAME:
+            break
+        print('waiting until connect, current output device is:', curr_device)
+        time.sleep(1)
+
+def is_connected(device_id):
+    return co(['blueutil',  '--is-connected', device_id]) == '0'
+
+
+airpods = get_id(AIRPODS_NAME)
 
 
 while True:
     if current_output() != AIRPODS_NAME:
         print('triggered')
-        if not is_connected:
-            print('disconnect')
-            disconnect(airpods['address'])
-        print('connect')
-        connect(airpods['address'])
-
-        while True:
-            curr_device = current_output()
-            print('waiting reconnect', curr_device)
-            if curr_device == AIRPODS_NAME:
-                print(curr_device == AIRPODS_NAME)
-                continue
-            time.sleep(1)
-
+        disconnect(airpods)
+        connect(airpods)
         print('pressing PLAY')
         PyKeyboard().press_key('KEYTYPE_PLAY') # resume playback
-    else:
-        print('all good')
+    print('all good' + ' ' * int(str(int(time.time()))[-1]) + '*')
     time.sleep(1)  
